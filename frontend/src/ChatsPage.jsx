697280d274911536c6f1182dc0c20d2f689ae77f
mike-melchior-school <mike.melchior@dcmail.ca>
2024-06-20 08:42:39 -0400

    +++

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,15 @@
-const ChatsPage = () => {
-    return <div className="background">chats...</div>
+import { PrettyChatWindow } from 'react-chat-engine-pretty'
+
+const ChatsPage = (props) => {
+    return (
+        <div style={{ height: '100vh'}}>
+            <PrettyChatWindow 
+                projectId='b8ba9108-246b-46b4-a1d7-802d8effc8a1'
+                username={props.user.username}
+                secret={props.user.secret}
+                style={{ height: '100%'}}/>
+        </div>
+    )
 }
 
 export default ChatsPage
